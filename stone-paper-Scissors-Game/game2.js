801aaefcb7897b19e8aecf2269d346c11a869353
avineshsharma <jangraavinesh@gmail.com>
2024-12-23 15:27:04 +0530

    {commit_message}

---
+++ stone-paper-Scissors-Game/game2.js
@@ -0,0 +1,32 @@
+// let age =prompt("Enter your age")
+// age=Number.parseInt(age)
+// let runagain =true;
+// while(runagain){
+//     const candrive = (age)=>{
+//        return  age>=18?true:false
+//     }
+//     if(age<0){
+//         console.error("invalid age")
+//         break;
+
+//     }
+// if(candrive(age)){
+//     alert("you can drive")
+// }
+
+// else{
+//     alert("you can't drive")
+// }
+// runagain=confirm("Do you want to run again?")
+// }
+
+
+// change url in browser
+// alert(location.href);
+// let num = prompt("enter the number");
+// num = Number.parseInt(num);
+// if (num >= 4) {
+//     location.href = "https://javascript.info/browser-environment";
+// }
+let color=prompt("enter the color");
+document.body.style.backgroundColor=color;
